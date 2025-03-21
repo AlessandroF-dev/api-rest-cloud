a849581547f06ce21c3bf68867e5f4832b00d165
afernandes <afernandesdev@outlook.com>
2025-03-20 22:40:53 -0300

    model base

--- src/main/java/br/com/cloud/rest/api/ApiRestCloudApplication.java
+++ src/main/java/br/com/cloud/rest/api/ApiRestCloudApplication.java
@@ -1,4 +1,4 @@
-package br.com.api_rest_cloud;
+package br.com.cloud.rest.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
