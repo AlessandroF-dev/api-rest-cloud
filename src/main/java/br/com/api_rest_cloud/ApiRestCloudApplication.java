ce07a96f167d634e1ffb2f1f8d5bd2f71cecc56d
afernandes <afernandesdev@outlook.com>
2025-03-20 22:22:40 -0300

    inicial do projeto

---
+++ src/main/java/br/com/api_rest_cloud/ApiRestCloudApplication.java
@@ -0,0 +1,13 @@
+package br.com.api_rest_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api_rest_cloud/ApiRestCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api_rest_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
